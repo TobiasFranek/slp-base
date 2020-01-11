cfe6d0dfbee81231269524682bb143fa2922629b
Tobias Franek <tfranek.mmt-b2018@fh-salzburg.ac.at>
2020-01-11 11:22:50 +0100

    added gallery and overlay

--- src/js/main.js
+++ src/js/main.js
@@ -9,4 +9,23 @@ accordion.addEventListener('click', (e) => {
 	arrow.classList.toggle('accordion__header__arrow--closed');
 	body.classList.toggle('accordion__body--closed');
 	accordion.classList.toggle('accordion__header--closed');
+});
+
+const galleryItems = document.querySelectorAll('.gallery__item');
+const overlay = document.querySelector('.overlay');
+const closeOverlay = document.querySelector('.overlay__close');
+
+closeOverlay.addEventListener('click', () => {
+	overlay.classList.toggle('overlay--closed');
 })
+
+for (let item of galleryItems) {
+	item.addEventListener('click', (e) => {
+		e.preventDefault();
+		const path = e.target.parentNode.getAttribute('href');
+		if (path) {
+			document.querySelector('.overlay__body').innerHTML = `<img src="${path}" class="gallery__item__image gallery__item__image--overlay" >`;
+			overlay.classList.toggle('overlay--closed');
+		}
+	});
+}
