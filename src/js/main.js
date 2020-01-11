44cef9b19c69687f0ca9f23fbcb4c80b19ee7815
Tobias Franek <tfranek.mmt-b2018@fh-salzburg.ac.at>
2020-01-11 10:25:44 +0100

    added accordion

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 import '../scss/main.scss';
+
+
+const accordion = document.querySelector('.accordion__header');
+
+accordion.addEventListener('click', (e) => {
+	const arrow = document.querySelector('.accordion__header__arrow');
+	const body = document.querySelector('.accordion__body');
+	arrow.classList.toggle('accordion__header__arrow--closed');
+	body.classList.toggle('accordion__body--closed');
+	accordion.classList.toggle('accordion__header--closed');
+})
